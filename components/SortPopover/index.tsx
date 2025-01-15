af59a08b552b67d4ea1fb69bf57443c11b9e65cf
yoh <yokeus2000@gmail.com>
2025-01-15 20:31:52 +0900

    💄 Update sort component styles

--- components/SortPopover/index.tsx
+++ components/SortPopover/index.tsx
@@ -9,6 +9,13 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import {
   Form,
   FormControl,
   FormField,
@@ -21,17 +28,26 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 
 import { toast } from "@/hooks/use-toast";
 import { ArrowUpDown, X } from "lucide-react";
 import { SortSchema } from "./schema";
 import { orderOptions, sortOptions } from "./data";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export const SortPopover = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [isOpen, setIsOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width: 640px)");
 
   const form = useForm<z.infer<typeof SortSchema>>({
     resolver: zodResolver(SortSchema),
@@ -69,118 +85,132 @@ export const SortPopover = () => {
   const currentSort = form.watch("sortBy");
   const currentOrder = form.watch("order");
 
+  const SortContent = () => (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="sortBy"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-sm font-semibold">
+                ソート項目
+              </FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Please select" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {sortOptions.map((option, i) => (
+                    <SelectItem
+                      key={i}
+                      value={option.value}
+                      className="flex items-center space-x-3 space-y-0"
+                    >
+                      {option.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Separator />
+        <FormField
+          control={form.control}
+          name="order"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-sm font-semibold">ソート順</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex space-x-4"
+                >
+                  {orderOptions.map((option) => (
+                    <FormItem
+                      key={option.value}
+                      className="flex items-center space-x-2 space-y-0"
+                    >
+                      <FormControl>
+                        <RadioGroupItem value={option.value} />
+                      </FormControl>
+                      <FormLabel className="font-normal">
+                        {option.label}
+                      </FormLabel>
+                    </FormItem>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          ソート適用
+        </Button>
+      </form>
+    </Form>
+  );
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>
+          <Button variant="outline" size={"icon"}>
+            <ArrowUpDown className="h-4 w-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={"bottom"}>
+          <SheetHeader>
+            <SheetTitle>ソート設定</SheetTitle>
+          </SheetHeader>
+          <SortContent />
+        </SheetContent>
+      </Sheet>
+    );
+  }
+
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
       <PopoverTrigger asChild>
-        <Button variant="outline" className="justify-start">
-          <ArrowUpDown className="md:mr-2 h-4 w-4" />
-          <span className="hidden md:block">
-            ソート
-            {currentSort && currentOrder && (
-              <span className="ml-2 text-xs opacity-50">
-                {
-                  sortOptions.find((option) => option.value === currentSort)
-                    ?.label
-                }
-                (
-                {
-                  orderOptions.find((option) => option.value === currentOrder)
-                    ?.label
-                }
-                )
-              </span>
-            )}
-          </span>
+        <Button variant="outline" className="w-[200px] justify-start">
+          <ArrowUpDown className="mr-2 h-4 w-4" />
+          ソート
+          {currentSort && currentOrder && (
+            <span className="ml-auto text-xs opacity-50">
+              {
+                sortOptions.find((option) => option.value === currentSort)
+                  ?.label
+              }
+              (
+              {
+                orderOptions.find((option) => option.value === currentOrder)
+                  ?.label
+              }
+              )
+            </span>
+          )}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-80">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <div className="flex items-center justify-between">
-              <h4 className="font-medium leading-none">ソート設定</h4>
-              <Button
-                variant="ghost"
-                size="sm"
-                className="h-8 w-8 p-0"
-                onClick={() => setIsOpen(false)}
-              >
-                <span className="sr-only">閉じる</span>
-                <X className="h-4 w-4" />
-              </Button>
-            </div>
-            <Separator />
-            <FormField
-              control={form.control}
-              name="sortBy"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-sm font-semibold">
-                    ソート項目
-                  </FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                      className="flex flex-col space-y-1"
-                    >
-                      {sortOptions.map((option) => (
-                        <FormItem
-                          key={option.value}
-                          className="flex items-center space-x-3 space-y-0"
-                        >
-                          <FormControl>
-                            <RadioGroupItem value={option.value} />
-                          </FormControl>
-                          <FormLabel className="font-normal">
-                            {option.label}
-                          </FormLabel>
-                        </FormItem>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Separator />
-            <FormField
-              control={form.control}
-              name="order"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-sm font-semibold">
-                    ソート順
-                  </FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                      className="flex space-x-4"
-                    >
-                      {orderOptions.map((option) => (
-                        <FormItem
-                          key={option.value}
-                          className="flex items-center space-x-2 space-y-0"
-                        >
-                          <FormControl>
-                            <RadioGroupItem value={option.value} />
-                          </FormControl>
-                          <FormLabel className="font-normal">
-                            {option.label}
-                          </FormLabel>
-                        </FormItem>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="w-full">
-              ソート適用
-            </Button>
-          </form>
-        </Form>
+        <div className="flex items-center justify-between mb-4">
+          <h4 className="font-medium leading-none">ソート設定</h4>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="h-8 w-8 p-0"
+            onClick={() => setIsOpen(false)}
+          >
+            <span className="sr-only">閉じる</span>
+            <X className="h-4 w-4" />
+          </Button>
+        </div>
+        <SortContent />
       </PopoverContent>
     </Popover>
   );
