6347f3cc3840f9da2f08a149d6e5dc84d63ac7b7
yoh <yokeus2000@gmail.com>
2025-01-23 19:43:59 +0900

    ♻️ Rename DrawerMenu component and this app name

--- components/DrawerMenu.tsx
+++ components/DrawerMenu.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import * as React from "react";
-
+import { AppConfig } from "@/app.config";
 import { Button } from "@/components/ui/button";
 import {
   Drawer,
@@ -14,26 +13,27 @@ import {
 } from "@/components/ui/drawer";
 import { Menu } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
-export const DrawerDemo = () => {
-  const [open, setOpen] = React.useState(false);
+export const DrawerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
+    <Drawer open={isOpen} onOpenChange={setIsOpen}>
       <DrawerTrigger asChild>
         <Button variant="ghost" size="icon" className="shrink-0">
           <Menu size={16} />
         </Button>
       </DrawerTrigger>
       <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>GOWANTED</DrawerTitle>
+        <DrawerHeader>
+          <DrawerTitle>{AppConfig.title}</DrawerTitle>
         </DrawerHeader>
-        <div className="flex flex-col space-y-1 px-4">
-          <Button variant={"secondary"} asChild>
+        <div className="flex flex-col space-y-2 px-4">
+          <Button variant={"link"} asChild>
             <Link href={"/"}>HOME</Link>
           </Button>
-          <Button variant={"secondary"} asChild>
+          <Button variant={"link"} asChild>
             <Link href={"/history"}>HISTORY</Link>
           </Button>
         </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
+import { AppConfig } from "@/app.config";
+
 export const Footer = () => {
   return (
     <footer className="sticky top-full h-16 px-10 py-6">
       <div className="flex items-center justify-center text-sm text-muted-foreground">
-        <span>&copy; 2024 GOWANTED</span>
+        <span>&copy; 2024 {AppConfig.title}</span>
       </div>
     </footer>
   );
